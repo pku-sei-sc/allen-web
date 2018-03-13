9fa3e5bff162bfe7dc3e896f32ccb4a810d6fd77
shawn <tangshuangpku@hotmail.com>
2018-03-13 20:30:03 +0800

    + 添加ElementUI，简单搭建UI框架

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container class="container">
+      <el-aside class="aside">
+        <!--<img src="./assets/logo.png">-->
+        <el-menu>
+          <el-menu-item>数据</el-menu-item>
+          <el-menu-item>模型</el-menu-item>
+          <el-menu-item>检测</el-menu-item>
+        </el-menu>
+      </el-aside>
+
+      <el-main class="main">
+        <router-view/>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -12,12 +24,22 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  .container {
+    height: 500px;
+    /*border: 1px solid #eee;*/
+  }
+  .aside {
+    /*background: aqua;*/
+    max-width: 150px;
+  }
+  .main {
+    background: aliceblue;
+  }
 </style>
